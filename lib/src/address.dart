35b3364b9d7578a6c79b70d3fe324328babdc6b0
peipeiTu <37138187+peipeiTu@users.noreply.github.com>
2022-04-12 18:16:00 +0800

    feature: support trx address

--- lib/src/address.dart
+++ lib/src/address.dart
@@ -1,12 +1,18 @@
 import 'dart:typed_data';
 
 import 'package:bs58check/bs58check.dart' as bs58check;
+import 'package:pointycastle/export.dart';
 
+import 'package:bitcoin_flutter/src/base58.dart';
+import 'package:bitcoin_flutter/src/bip32/bip32.dart';
 import 'package:bitcoin_flutter/src/models/networks.dart';
 import 'package:bitcoin_flutter/src/payments/index.dart' show PaymentData;
 import 'package:bitcoin_flutter/src/payments/p2pkh.dart';
 import 'package:bitcoin_flutter/src/payments/p2sh.dart';
 import 'package:bitcoin_flutter/src/payments/p2wpkh.dart';
+import 'package:bitcoin_flutter/src/utils/constants/constants.dart';
+
+final _domainParams = ECCurve_secp256k1();
 
 class Address {
   static bool validateAddress(String address, [NetworkType? nw]) {
@@ -42,5 +48,28 @@ class Address {
       }
     }
     throw ArgumentError('$address has no matching Script');
+  }
+
+  static String createExtendedAddress(Uint8List seed, {String? path, List<int>? prefix}) {
+    if (path == null) path = "m/44'/195'/0'/0/0";
+    if (prefix == null) prefix = xprv;
+
+    final root = ExtendedPrivateKey.master(seed, prefix);
+    final r = root.forPath(path);
+
+    final q = _domainParams.G * (r as ExtendedPrivateKey).key;
+
+    final publicParams = ECPublicKey(q, _domainParams);
+    final pk = publicParams.Q!.getEncoded(false);
+
+    final input = Uint8List.fromList(pk.skip(1).toList());
+
+    final digest = KeccakDigest(256);
+    final result = Uint8List(digest.digestSize);
+    digest.update(input, 0, input.length);
+    digest.doFinal(result, 0);
+
+    final addr = result.skip(result.length - 20).toList();
+    return Base58CheckCodec.bitcoin().encode(Base58CheckPayload(0x41, addr));
   }
 }
